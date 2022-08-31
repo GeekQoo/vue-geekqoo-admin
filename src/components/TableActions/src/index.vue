ec26e5980f7cbc07f74c045a320bd001ae59ec21
xuqiaolou <398622064@qq.com>
2022-08-31 17:53:28 +0800

    :sparkles: feat:TableActions组件新增自定义颜色字段

--- src/components/TableActions/src/index.vue
+++ src/components/TableActions/src/index.vue
@@ -3,6 +3,7 @@
         <template v-for="item in actions">
             <n-button
                 v-if="!item.actionType || item.actionType === 'button'"
+                :color="item.color || undefined"
                 :disabled="item.disabled ? item.disabled() : false"
                 :type="item.type || 'primary'"
                 :size="item.size || 'small'"
--- src/components/TableActions/src/types.ts
+++ src/components/TableActions/src/types.ts
@@ -1,8 +1,9 @@
 export type TableActionsProps = {
-    actionType?: "button";
-    label: string;
-    type?: "default" | "tertiary" | "primary" | "success" | "info" | "warning" | "error";
-    size?: "tiny" | "small" | "medium" | "large";
-    onClick?: () => void;
-    disabled?: () => boolean;
+    actionType?: "button"; // 操作类型，默认为button
+    label: string; // 操作名称
+    type?: "default" | "tertiary" | "primary" | "success" | "info" | "warning" | "error"; // 按钮类型
+    color?: string; // 按钮颜色，设定 color 后会覆盖 type 的样式
+    size?: "tiny" | "small" | "medium" | "large"; // 按钮大小
+    onClick?: () => void; // 按钮点击事件
+    disabled?: () => boolean; // 按钮是否禁用
 }[];
