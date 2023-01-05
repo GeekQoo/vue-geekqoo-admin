927e8b3da489f7ebd6f73f1ad431c352142f7ce1
xuqiaolou <398622064@qq.com>
2023-01-05 15:16:14 +0800

    :rocket: perf:优化用户列表页/菜单列表页按钮展示

--- src/views/Container/Permission/Menu/MenuList.vue
+++ src/views/Container/Permission/Menu/MenuList.vue
@@ -10,8 +10,8 @@
         </n-card>
         <n-card class="mt" hoverable>
             <n-space class="mb">
-                <n-button type="primary">新增</n-button>
-                <n-button type="error">删除</n-button>
+                <n-button secondary type="primary">新增</n-button>
+                <n-button secondary type="error">删除</n-button>
             </n-space>
             <n-data-table
                 :columns="tableColumns"
@@ -33,7 +33,7 @@
 import { h, onMounted, reactive, ref } from "vue";
 import type { DataTableColumns } from "naive-ui";
 import type { TableSearchbarConfig, TableSearchbarData, TableSearchbarOptions } from "@/components/Table";
-import { TableSearchbar, TableActions } from "@/components/Table";
+import { TableActions, TableSearchbar } from "@/components/Table";
 import { DynamicIcon } from "@/components/DynamicIcon";
 import { GET_MENU_LIST } from "@/api/permission/menu";
 import { useCommonTable } from "@/hooks";
@@ -119,12 +119,14 @@ let tableColumns = ref<DataTableColumns<RowProps>>([
                 buttonActions: [
                     {
                         label: "编辑",
+                        tertiary: true,
                         onClick: () => {
                             console.log(111, row);
                         }
                     },
                     {
                         label: "删除",
+                        tertiary: true,
                         type: "error",
                         onClick: () => {
                             console.log(222, row);
--- src/views/Container/Permission/User/UserList.vue
+++ src/views/Container/Permission/User/UserList.vue
@@ -10,13 +10,13 @@
         </n-card>
         <n-card class="mt" hoverable>
             <n-space class="mb">
-                <n-button type="primary">新增</n-button>
-                <n-button type="error">删除</n-button>
+                <n-button secondary type="primary">新增</n-button>
+                <n-button secondary type="error">删除</n-button>
             </n-space>
             <n-data-table
-                :loading="tableLoading"
                 :columns="tableColumns"
                 :data="tableData"
+                :loading="tableLoading"
                 :pagination="tablePagination"
                 :row-key="tableRowKey"
                 :single-line="false"
@@ -33,7 +33,7 @@
 import { h, onMounted, reactive, ref } from "vue";
 import type { DataTableColumns } from "naive-ui";
 import type { TableSearchbarConfig, TableSearchbarData, TableSearchbarOptions } from "@/components/Table";
-import { TableSearchbar, TableActions } from "@/components/Table";
+import { TableActions, TableSearchbar } from "@/components/Table";
 import { GET_USER_LIST } from "@/api/permission/user";
 import { useCommonTable } from "@/hooks";
 
@@ -147,12 +147,14 @@ let tableColumns = ref<DataTableColumns<RowProps>>([
                 buttonActions: [
                     {
                         label: "编辑",
+                        tertiary: true,
                         onClick: () => {
                             console.log(111, row);
                         }
                     },
                     {
                         label: "删除",
+                        tertiary: true,
                         type: "error",
                         onClick: () => {
                             console.log(222, row);
