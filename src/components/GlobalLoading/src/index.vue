5a26744c522b9af9bc7f06e449c97052ebe0f2a7
xuqiaolou <398622064@qq.com>
2023-02-03 16:35:11 +0800

    :rocket: perf:优化unocss配置

--- src/components/GlobalLoading/src/index.vue
+++ src/components/GlobalLoading/src/index.vue
@@ -1,15 +1,5 @@
 <template>
-    <div class="global-loading flex-center fixed top-0 left-0 w-screen h-screen z-99999">
-        <n-result status="418" title="加载中" description="喝杯茶吧~" size="large">
-            <!--<template #icon>-->
-            <!--    <n-spin size="large" />-->
-            <!--</template>-->
-        </n-result>
+    <div class="flex-center fixed-tl wh-screen z-99999 bg-[#FFFFFF]">
+        <n-result description="喝杯茶吧~" size="large" status="418" title="加载中" />
     </div>
 </template>
-
-<style lang="scss" scoped>
-.global-loading {
-    background: #ffffff;
-}
-</style>
--- unocss.config.ts
+++ unocss.config.ts
@@ -4,11 +4,7 @@ export default defineConfig({
     exclude: ["node_modules", ".git", ".idea", "dist", "public", "build"],
     rules: [
         [/^flex-fixed-(\d+)$/, ([, d]) => ({ flex: `0 0 ${d}px` })],
-        [/^wh-(.+)$/, ([, d]) => ({ width: `${d}`, height: `${d}` })],
-        [/^ptb-(.+)$/, ([, d]) => ({ "padding-top": `${d}`, "padding-bottom": `${d}` })],
-        [/^plr-(.+)$/, ([, d]) => ({ "padding-left": `${d}`, "padding-right": `${d}` })],
-        [/^mtb-(.+)$/, ([, d]) => ({ "margin-top": `${d}`, "margin-bottom": `${d}` })],
-        [/^mlr-(.+)$/, ([, d]) => ({ "margin-left": `${d}`, "margin-right": `${d}` })]
+        [/^wh-(.+)$/, ([, d]) => ({ width: `${d}`, height: `${d}` })]
     ],
     shortcuts: {
         "flex-center": "flex justify-center items-center",
@@ -18,6 +14,11 @@ export default defineConfig({
         "absolute-tr": "absolute top-0 right-0 ",
         "absolute-bl": "absolute bottom-0 left-0",
         "absolute-br": "absolute bottom-0 right-0",
-        "transition-base": "transition-all duration-300 ease-in-out"
+        "fixed-tl": "fixed left-0 top-0",
+        "fixed-tr": "fixed right-0 top-0",
+        "fixed-bl": "fixed left-0 bottom-0",
+        "fixed-br": "fixed right-0 bottom-0",
+        "transition-base": "transition-all duration-300 ease-in-out",
+        "wh-screen": "w-screen h-screen"
     }
 });
