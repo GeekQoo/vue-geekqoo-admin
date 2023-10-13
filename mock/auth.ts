9cb1c6870e8e9cbfd28ceec94ba68ef6b81191f7
SniperXu <398622064@qq.com>
2023-10-13 15:30:48 +0800

    :sparkles: feat:新增流程管理演示页

--- mock/auth.ts
+++ mock/auth.ts
@@ -60,6 +60,19 @@ export default [
                                 type: 2
                             }
                         ]
+                    },
+                    {
+                        label: "流程管理",
+                        key: "2",
+                        type: 1,
+                        icon: "DeploymentUnitOutlined",
+                        children: [
+                            {
+                                label: "流程设计",
+                                key: "Flow",
+                                type: 2
+                            }
+                        ]
                     }
                 ]
             },
---
+++ src/views/Container/Flow/Flow.vue
@@ -0,0 +1,12 @@
+<template>
+    <n-card hoverable>
+        <div id="modeler-container" />
+    </n-card>
+</template>
+
+<script lang="ts" setup>
+import "bpmn-js/dist/assets/diagram-js.css";
+import "bpmn-js/dist/assets/bpmn-font/css/bpmn.css";
+import "bpmn-js/dist/assets/bpmn-font/css/bpmn-codes.css";
+import "bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css";
+</script>
