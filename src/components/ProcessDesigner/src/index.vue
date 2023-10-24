50c1988016c3dd832f75ca2165f78833730a7163
SniperXu <398622064@qq.com>
2023-10-24 23:46:03 +0800

    :sparkles: feat:基于Bpmnjs新增流程设计组件（开发中）

--- src/components/ProcessDesigner/src/index.vue
+++ src/components/ProcessDesigner/src/index.vue
@@ -6,17 +6,38 @@
 </template>
 
 <script lang="ts" setup>
-import { onMounted, onUnmounted, watch } from "vue";
+import { onMounted, onUnmounted } from "vue";
 import { useThrottleFn } from "@vueuse/core";
 import translate from "@/plugins/bpmn/i18n";
 import BpmnModeler from "bpmn-js/lib/Modeler";
 import { BpmnPropertiesPanelModule, BpmnPropertiesProviderModule } from "bpmn-js-properties-panel";
+import dayjs from "dayjs";
 
-let props = withDefaults(defineProps<{ xml: string }>(), {
-    xml: ""
-});
+let props = withDefaults(defineProps<{ xml: string }>(), { xml: "" });
 
 let emits = defineEmits(["update:xml"]);
+
+// 创建空xml
+let createEmptyXml = (
+    processId: string,
+    processName: string,
+    category: string = "http://bpmn.io/schema/bpmn",
+    description: string = ""
+) => {
+    return `<?xml xml=string version="2.0" encoding="UTF-8"?>
+    <bpmn:definitions xmlns:flowable="http://flowable.org/bpmn" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" targetNamespace="${category}">
+      <bpmn:process id="${processId}" name="${processName}" isExecutable="true">
+          <bpmn:documentation>${description}</bpmn:documentation>
+      </bpmn:process>
+      <bpmndi:BPMNDiagram id="BPMNDiagram_1">
+        <bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="Process_1">
+          <bpmndi:BPMNShape id="_BPMNShape_StartEvent_2" bpmnElement="StartEvent_1">
+            <dc:Bounds height="36.0" width="36.0" x="160.0" y="160.0"/>
+          </bpmndi:BPMNShape>
+        </bpmndi:BPMNPlane>
+      </bpmndi:BPMNDiagram>
+    </bpmn:definitions>`;
+};
 
 // 建模
 let modeler: any = null;
@@ -25,9 +46,8 @@ let onModeling = async () => {
     [modeler] = await Promise.all([
         new BpmnModeler({
             container: "#canvas",
-            propertiesPanel: {
-                parent: "#properties"
-            },
+            keyboard: { bindTo: window },
+            propertiesPanel: { parent: "#properties" },
             additionalModules: [
                 BpmnPropertiesPanelModule,
                 BpmnPropertiesProviderModule,
@@ -37,39 +57,36 @@ let onModeling = async () => {
             moddleExtensions: {}
         })
     ]);
-    await modeler.createDiagram();
+    if (props.xml) {
+        await modeler.importXML(props.xml);
+    } else {
+        let nowTime = dayjs().valueOf();
+        await modeler.importXML(createEmptyXml(`id${nowTime}`, `流程${nowTime}`));
+    }
+    onWatchModeler();
 };
 
 onMounted(async () => {
     await onModeling();
-    if (props.xml) {
-        try {
-            await modeler.importXML(props.xml);
-        } catch (err) {
-            console.log("err", err);
-        }
-    }
 });
 
-// 流程变更
-watch(
-    () => modeler,
-    (val) => {
-        if (val) {
-            val.on("commandStack.changed", modelerChange);
+// 流程监听
+let onWatchModeler = () => {
+    // 自动更新xml
+    modeler.on("commandStack.changed", onAutoSave);
+    modeler.on("selection.changed", (e: any) => {
+        if (e.newSelection.length > 0) {
+            console.log(1, e.newSelection[0]);
+            let modeling = modeler.get("modeling");
+            // modeling.updateProperties(e.newSelection[0], { id: "test", name: "测试" });
         }
-    }
-);
+    });
+};
 
-let modelerChange = useThrottleFn(() => {
-    onSave();
-}, 1000);
-
-// 保存
-let onSave = async () => {
+let onAutoSave = useThrottleFn(async () => {
     let { xml } = await modeler.saveXML({ format: true });
     emits("update:xml", xml);
-};
+}, 1000);
 
 onUnmounted(() => {
     modeler.destroy();
