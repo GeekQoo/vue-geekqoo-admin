66b029239b1b0ce8c3e1e310845ca1fa2a5ed3bd
SniperXu <398622064@qq.com>
2023-10-26 16:49:50 +0800

    :sparkles: feat:基于Bpmnjs新增流程设计组件（开发中）

--- src/components/ProcessDesigner/src/components/NodeConfig.vue
+++ src/components/ProcessDesigner/src/components/NodeConfig.vue
@@ -13,6 +13,13 @@
                     <n-form-item label="节点ID:">
                         <n-input :default-value="id" disabled />
                     </n-form-item>
+                    <n-form-item label="审批人员:">
+                        <n-select
+                            :options="userSelect.options"
+                            v-model:value="userSelect.value"
+                            @update:value="changeUserSelect"
+                        />
+                    </n-form-item>
                 </n-form>
             </n-drawer-content>
         </n-drawer>
@@ -20,9 +27,16 @@
 </template>
 
 <script lang="ts" setup>
-import { computed } from "vue";
+import { computed, ref } from "vue";
 
-let props = withDefaults(defineProps<{ show: boolean; id: string; modeler: any }>(), { show: () => false });
+let props = withDefaults(
+    defineProps<{
+        show: boolean;
+        id: string;
+        modeler: any;
+    }>(),
+    { show: () => false }
+);
 
 let emits = defineEmits(["update:show"]);
 
@@ -30,5 +44,28 @@ let show = computed({ get: () => props.show, set: (val) => changeModalShow(val) 
 
 let changeModalShow = (show: boolean) => {
     emits("update:show", show);
+};
+
+// 模拟审批人
+interface UserSelectProps {
+    options: Record<string, string>[];
+    value: Nullable<string>;
+}
+
+let userSelect = ref<UserSelectProps>({
+    options: [
+        { label: "张三", value: "1" },
+        { label: "李四", value: "2" },
+        { label: "王五", value: "3" }
+    ],
+    value: null
+});
+
+let changeUserSelect = (value: string, obj: Record<string, string>) => {
+    let modeling = props.modeler.get("modeling");
+    let elementRegistry = props.modeler.get("elementRegistry");
+    modeling.updateProperties(elementRegistry._elements[props.id].element, {
+        name: `${obj.label}审批`
+    });
 };
 </script>
