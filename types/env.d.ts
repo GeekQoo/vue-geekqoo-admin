8d1c58799c3cc8350dc78b52d0fd35e9d0497760
SniperXu <398622064@qq.com>
2024-11-29 16:29:45 +0800

    :rocket: perf:删除mock相关内容

--- types/env.d.ts
+++ types/env.d.ts
@@ -2,7 +2,6 @@ interface ImportMetaEnv {
     readonly VITE_BASE_PATH: string;
     readonly VITE_OUTPUT_DIR: string;
     readonly VITE_API_URL: string;
-    readonly VITE_USE_MOCK: string;
 }
 
 interface ImportMeta {
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import { resolve } from "path";
 import vue from "@vitejs/plugin-vue";
-import { viteMockServe } from "vite-plugin-mock";
 import autoprefixer from "autoprefixer";
 import unocss from "unocss/vite";
 
@@ -9,17 +8,7 @@ export default defineConfig(({ command, mode }) => {
     const viteEnv = loadEnv(mode, process.cwd(), "");
 
     return {
-        plugins: [
-            vue(),
-            unocss(),
-            viteMockServe({
-                mockPath: "mock",
-                localEnabled: command === "serve",
-                prodEnabled: command !== "serve",
-                //  这样可以控制关闭mock的时候不让mock打包到最终代码内
-                injectCode: `import { setupProdMockServer } from "../mock/mockProdServer";setupProdMockServer();`
-            })
-        ],
+        plugins: [vue(), unocss()],
         base: viteEnv.VITE_BASE_PATH, // 开发或生产环境服务的公共基础路径。
         publicDir: "public", // 作为静态资源服务的文件夹。
         resolve: {
@@ -36,6 +25,11 @@ export default defineConfig(({ command, mode }) => {
             cssTarget: "chrome80" // 此选项允许用户为 CSS 的压缩设置一个不同的浏览器 target，此处的 target 并非是用于 JavaScript 转写目标。
         },
         css: {
+            preprocessorOptions: {
+                scss: {
+                    api: "modern-compiler"
+                }
+            },
             postcss: {
                 plugins: [
                     autoprefixer({
@@ -45,16 +39,16 @@ export default defineConfig(({ command, mode }) => {
             }
         },
         server: {
-            host: true, // 指定服务器应该监听哪个 IP 地址。 如果将此设置为 0.0.0.0 或者 true 将监听所有地址，包括局域网和公网地址。
+            host: false, // 指定服务器应该监听哪个 IP 地址。 如果将此设置为 0.0.0.0 或者 true 将监听所有地址，包括局域网和公网地址。
             port: 23333, // 指定开发服务器端口。
-            open: false, // 是否自动打开浏览器。
+            open: true, // 是否自动打开浏览器。
             cors: false, // 为开发服务器配置 CORS。
             proxy: {
-                // "/api": {
-                //     target: "http://demo.demo",
-                //     changeOrigin: true,
-                //     ws: true
-                // }
+                "/api": {
+                    target: "http://localhost:3000",
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/api/, "")
+                }
             }
         }
     };
